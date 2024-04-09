ada6ca4d0948507dfa363af7bed13fb72f56c43e
PretzelOptician <alekschultz1@gmail.com>
2024-04-08 21:45:57 -0400

    Notes page done

--- database-requests.php
+++ database-requests.php
@@ -24,4 +24,18 @@ function getCoursesForDept($dept_code)
    return $result;
 }
 
+function getNotesForCourse($course_id) {
+   global $db;
+   $query = "SELECT N.id AS note_id, N.date_uploaded, U.name AS author_name, N.computing_id, AVG(R.value) AS average_rating 
+            FROM Note N JOIN User U ON N.computing_id = U.computing_id LEFT JOIN NoteRating NR ON N.id = NR.note_id LEFT JOIN Rating R ON NR.rating_id = R.id 
+            WHERE N.course_id = " . $course_id . " 
+            GROUP BY N.id, N.date_uploaded, U.name, N.computing_id;";
+   $statement = $db->prepare($query);    // compile
+   $statement->execute();
+   $result = $statement->fetchAll();     // fetch()
+   $statement->closeCursor();
+
+   return $result;
+}
+
 ?>
